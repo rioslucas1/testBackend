b4a93f0a86d33df9462e4395ea656d9313302dd9
rioslucas1 <rioslucasariel01@gmail.com>
2024-12-06 00:30:13 -0300

    TestBackendGrava

--- bin/init.js
+++ bin/init.js
@@ -1,13 +1,18 @@
 'use strict';
-require('dotenv').config();
+const path = require('path');
+const env =   require('dotenv').config({ path: path.resolve(__dirname, '../dbConfig.env') });
 const app = require('../app');
 const logger = require('../lib/logger');
 
+
+console.log(env);
 return app.connectMongoose()
     .then(() => {
+        console.log("llegue hasta aca")
         const application = app.initialize();
-        application.listen(process.env.SERVER_PORT);
-        logger.info(`Your server is listening on port ${process.env.SERVER_PORT}`);
+        application.listen(process.env.SERVER_PORT, () => {
+            logger.info(`Your server is listening on port ${process.env.SERVER_PORT}`);
+        });
     })
     .catch((error) => {
         logger.error('APP STOPPED');
